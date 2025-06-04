4d614cedb554db7a9c753fadbc1915ee4838f745
Guilherme <gui.ferraresi2008@gmail.com>
2025-06-04 08:15:42 -0300

    Adicionando o banco

---
+++ banco.sql
@@ -0,0 +1,15 @@
+create database dbCadastrarProduto ;
+use dbCadastrarProduto;
+
+create table tbUsuarios(
+IdUser int primary key auto_increment,
+Nome varchar(50) not null,
+Email varchar(100) not null,
+Senha varchar(50) not null);
+
+create table tbProdutos(
+IdProd int primary key auto_increment,
+Nome varchar(50) not null,
+Descricao varchar(100) not null,
+Preco decimal(6,2) not null,
+Quantidade int not null);
