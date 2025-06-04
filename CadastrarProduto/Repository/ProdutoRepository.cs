74738ba2f8f8ac0946ae8489b41d962db1dd893c
Guilherme <gui.ferraresi2008@gmail.com>
2025-06-04 08:37:59 -0300

    Adicionando o codigo do repositorio

--- CadastrarProduto/Repository/ProdutoRepository.cs
+++ CadastrarProduto/Repository/ProdutoRepository.cs
@@ -1,6 +1,52 @@
-﻿namespace CadastrarProduto.Repository
+﻿using CadastrarProduto.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace CadastrarProduto.Repository
 {
-    public class ProdutoRepository
+    public class ProdutoRepository(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+        public Produto ObterProduto(string nome)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("SELECT * FROM tbProdutos WHERE Nome = @nome", conexao);
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = nome;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Produto produto = null;
+                    if (dr.Read())
+                    {
+                        produto = new Produto
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = dr["Nome"].ToString(),
+                            Descricao = dr["Descricao"].ToString(),
+                            Preco = Convert.ToDecimal(dr["Preco"]),
+                            Quantidade = Convert.ToInt32(dr["Quantidade"]),
+                        };
+                    }
+                    return produto;
+                }
+            }
+        }
+        public void AdicionarProduto(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("INSERT INTO tbProdutos (Nome, Descricao, Preco, Quantidade) VALUES (@Nome,@Descricao,@Preco,@Quantidade)", conexao);
+                cmd.Parameters.AddWithValue("@Nome", produto.Nome);
+                cmd.Parameters.AddWithValue("@Descricao", produto.Descricao);
+                cmd.Parameters.AddWithValue("@Preco", produto.Preco);
+                cmd.Parameters.AddWithValue("@Quantidade", produto.Quantidade);
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
     }
 }
--- CadastrarProduto/Repository/UsuarioRepository.cs
+++ CadastrarProduto/Repository/UsuarioRepository.cs
@@ -1,6 +1,50 @@
-﻿namespace CadastrarProduto.Repository
+﻿using CadastrarProduto.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace CadastrarProduto.Repository
 {
-    public class UsuarioRepository
+    public class UsuarioRepository(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+        public Usuario ObterUsuario(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("SELECT * FROM tbUsuarios WHERE Email = @email", conexao);
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Usuario usuario = null;
+                    if (dr.Read())
+                    {
+                        usuario = new Usuario
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = dr["Nome"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            Senha = dr["Senha"].ToString()
+                        };
+                    }
+                    return usuario;
+                }
+            }
+        }
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("INSERT INTO tbUsuarios (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)", conexao);
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
     }
 }
